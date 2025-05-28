aa917dc64634316c5630fa172637b30398bdb29d
oancholarevelo <oancholarevelo@gmail.com>
2025-05-28 22:56:20 +0800

    Speed Test

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Login from './components/Login';
 import Register from './components/Register';
 import PostureGraph from './components/PostureGraph';
