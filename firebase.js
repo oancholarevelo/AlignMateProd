a6b0e77ca20e251d14b2bdddcabd080d4365ac73
oancholarevelo <oancholarevelo@gmail.com>
2025-07-09 22:28:41 +0800

    Update

--- firebase.js
+++ firebase.js
@@ -1,28 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8E-haIMUsm5HTXLoEf9ho3yLYq9HJm0A",
-  authDomain: "test-alignmate.firebaseapp.com",
-  databaseURL: "https://test-alignmate-default-rtdb.firebaseio.com",
-  projectId: "test-alignmate",
-  storageBucket: "test-alignmate.firebasestorage.app",
-  messagingSenderId: "32530267491",
-  appId: "1:32530267491:web:97e813ac71ff8bd54c2756",
-  measurementId: "G-QQMG1H34WR"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const database = getDatabase(app); // Initialize database
+const database = getDatabase(app);
 
 export { auth, database };
